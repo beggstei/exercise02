228789d5f09b5599698dc238361dc57cf71c9595
beggstei <104060130+beggstei@users.noreply.github.com>
2022-06-13 16:10:08 +0200

    1

---
+++ code.js
@@ -0,0 +1,30 @@
+//const element = document.querySelector('[data-js="sitz"]');
+//element.classList.add("red");
+console.log("bndnd");
+//let x = 0;
+//delayTime(1000);
+//addEventListener
+//document.getElementById("carddd").style.backgroundColor = "red";
+
+document.getElementById("feldred").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "red";})
+document.getElementById("feldblue").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "blue";})
+document.getElementById("feldgreen").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "green";})
+document.getElementById("feldyellow").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "yellow";})
+document.getElementById("feldorange").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "orange";})
+document.getElementById("feldpink").addEventListener("click", function(){document.getElementById("carddd").style.backgroundColor = "magenta";})
+
+
+
+
+
+//document.getElementById("carddd").addEventListener("click", colorChange());
+/*
+function colorChange(){
+    console.log("enter");
+    while(x<255){
+        var thergb = "'rgb(" + x + "," + x + "," + x + ")'";
+        document.getElementById("carddd").style.backgroundColor = thergb;
+        console.log(thergb);
+        x++;
+    }
+}*/
